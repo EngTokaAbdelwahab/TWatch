848b01e7fe04547e1010d30106512e5f13f6cb64
EngTokaAbdelwahab <eng.tokabdelwahab@gmail.com>
2024-07-30 20:26:36 +0300

    first commit

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return <>
+   <div className="footer ">
+  <footer className='text-white text-center py-2 '>
+    <p>Copyright © 2024 Designed by TOKA. All rights reserved</p>
+  </footer>
+  </div>
+  </>
+}
+
---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Movie from '../Movie/Movie'
+import Tv from '../Tv/Tv'
+import Celebrities from '../Celebrities/Celebrities'
+
+export default function Home() {
+  return <>
+  
+
+  <Movie/>
+  <Tv/>
+  <Celebrities/>
+  
+ 
+  </>
+} 
